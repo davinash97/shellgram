63cda111999679c235c0b1fd6efe2ec39f0ece0b
davinash97 <avinashd2904@gmail.com>
2021-06-18 18:49:12 +0530

    Initial Commit

---
+++ telegram.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source tools/telegram.functions
+# Telegram Script to Send File(s) or Message(s) into Your Chat/Channel
+# Just Make Sure To add the Bot in respective Chat/Channel
+# By DAvinash97 for Developers
+
+# Your Bot Token Here
+BOT_TOKEN="s"
+
+# Your Chat ID Here
+CHAT_ID="s"
+
+# Add Commands Here
+sendmessage "Telegram is Awesome"
+senddocument "txt" "Don't Open it pliz"
